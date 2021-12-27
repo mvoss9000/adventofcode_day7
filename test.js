25838eb905b006589119eca3841c47ea0d2b4dfc
mvoss9000 <mvoss9000@yahoo.com>
2021-12-27 16:37:23 -0500

    initial release

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const test = require('test-kit').tape()
 const crabs = require('.')
 
-test('counts', function (t) {
+test.print('counts', function (t) {
     t.table_assert([
-        ['a', 'beg', 'end', 'exp'],
-        [[0,1,2], 1, 2, [3, 5]],
-        [[0,1,2,3], 1, 3, [6, 14]],
-        [[2,1,0], 1, 0, [3, 5]],
-        [[2,1,0], 2, 0, [3, 8]],
+        [ 'a',            'beg', 'end', 'exp' ],
+        [ [ 0, 1, 2 ],    1,     2,     [ 3, 5 ] ],
+        [ [ 0, 1, 2, 3 ], 1,     3,     [ 6, 14 ] ],
+        [ [ 2, 1, 0 ],    1,     0,     [ 3, 5 ] ],
+        [ [ 2, 1, 0 ],    2,     0,     [ 3, 8 ] ],
     ], crabs.counts)
 })
 
